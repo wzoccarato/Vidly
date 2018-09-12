07f10e80d90c22ae3370d51e6a0d4b11f9b86065
Walter Zoccarato <walter.zoccarato@siplus.it>
2018-09-12 05:27:17 +0200

    Sezione3, prima dei 3 esercizi finali

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;       // da inludere per utilizzate  l'estensione "Include" nel metodo Index
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -10,31 +11,38 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // e' necessario farlo perche' _context e' un oggetto "Disposable"
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            // con ToList la query non vine piu' eseguita in maniera differita.
+            // alcontrario, questa vine eseguita immediatamente, e ritorna subito
+            // la lista degli elementi
+            // Eager Loading
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            // anche questa query, naturalmente, viene eseguita immdiatamente
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
-        }
-
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,6 +10,4 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
     }
-
-    // /movies/random
 }
